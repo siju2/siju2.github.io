da1d375d0f3b2278a4fdb28af4907dc9b1cd2d92
siju2 <66425318+siju2@users.noreply.github.com>
2025-04-18 15:03:13 +0900

    Add files via upload

---
+++ HTML/R.r
@@ -0,0 +1,19 @@
+
+install.packages("dplyr")
+library(dplyr)
+
+arrange(, , )
+
+head(arrange(mtcars, wt))
+
+
+mpg(rank)
+rank(mpg)
+
+cy1 <- c(33123,421,4,1,2,51,61,6,57,467,3,745,56,3,23521,4,12)
+
+distinct(mtcars, cy1)
+
+summarise(mtcars, mean(cy1), min(cy1) , max(cy1) )
+
+summarise(gr_cy1, n())
